1d16b7bae7ed27cd9f43b6e1da91c336b94be982
cryptonocry <132585712+cryptonocry@users.noreply.github.com>
2025-07-15 01:34:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from aiogram.utils import executor
 import os
 
 TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "280665761"))
 
 bot = Bot(token=7665240651:AAHbJ4fBrNQxwcLFO-J1KEHGLTe18q4CaQ4)
 dp = Dispatcher(bot)
